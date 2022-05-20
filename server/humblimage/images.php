17d21947b543de23f750306114ea65da4fb4f9fd
Mahir Tekin <mterdensan@gmail.com>
2022-05-20 16:22:56 +0300

    testing search methods

--- server/humblimage/images.php
+++ server/humblimage/images.php
@@ -1,61 +1,133 @@
 <?php
 
-require __DIR__ . "../composer/vendor/autoload";
+require __DIR__ . "/../composer/vendor/autoload.php";
+require __DIR__."/enums.php";
 
 
-
+/**
+ * humblimage - images
+ *
+ * @category  Twitter Bot
+ * @package   humblimage
+ * @author    humbldump <humbldump@protonmail.com>
+ * @copyright Copyright (c) 2022
+ * @link      https://github.com/humbldump/humblimage
+ * @version   0.1.0
+ */
 class images
 {
-  use mysqliDB;
 
 
   /**
    * @var string unique id of the image
    */
-  public int $id;
+  public int $id = 0;
 
   /**
    * @var int Posted Tweet id of the image
    */
-  public int $tweet_id;
+  public int $tweet_id = 0;
 
   /**
    * @var string Image id from unsplash
    */
-  public string $image_id;
+  public string $image_id = "";
 
   /**
    * @var string Image description from unsplash
    */
-  public string $image_description;
+  public string $image_description = "";
 
   /**
    * @var string Image alternative decsription from unsplash
    */
-  public string $image_alt_descriotion;
+  public string $image_alt_description = "";
 
   /**
    * @var string Owner of the image id from unsplash
    */
-  public string $owner_id;
+  public string $owner_id = "";
 
   /**
    * @var string Owner username of unsplash
    */
-  public string $owner_username;
+  public string $owner_username = "";
 
   /**
    * @var string Owner fullname from unsplash
    */
-  public string $owner_name;
+  public string $owner_name = "";
 
   /**
    * @var string Owner twitter profile username
    */
-  public string $owner_twitter_username;
+  public string $owner_twitter_username = "";
 
   /**
    * @var DateTime of the Twitter post
    */
-  public DateTime $postedat;
+  public $postedat = null;
+
+
+
+
+
+  /**
+   * @var MysqliDB database connection
+   */
+  private MysqliDb $db;
+
+
+  public function __construct() {
+    
+    /* Call Static DB method to connecy Database */
+    $this->db = $this::connectDB();
+
+  }
+
+  public function SearchbyId(int $id): ?images{
+    $result = $this->db->where('id', $id)->getOne(enum::tables['images']);
+    return $this->assignData($result);
+  }
+
+  public function SearchbyImageId(string $image_id): ?images{
+    $result = $this->db->where('image_id', $image_id)->getOne(enum::tables['images']);
+    return $this->assignData($result);
+  }
+
+  public function SearchbyTweetId(int $tweet_id): ?images{
+    $result = $this->db->where('tweet_id', $tweet_id)->getOne(enum::tables['images']);
+    return $this->assignData($result);
+  }
+
+
+
+
+
+
+  /**
+   * It takes an array of data and assigns it to the object's properties.
+   * 
+   * @param array|null $data The array of data to be assigned to the object.
+   * 
+   * @return ?images The object itself.
+   */
+  private function assignData(array $data = null): ?images{
+
+    if ($data == null) {
+      throw new Exception("Couldn't find the selected image...", 1);
+    }
+
+    foreach ($data as $key => $value) {
+      if (property_exists($this, $key)) {
+        $this->$key = $value;
+      }
+    }
+
+    return $this;
+  }
+
+  public static function connectDB(): ?MysqliDb{
+    return new MysqliDb ('localhost', getenv('mysql-user'), getenv('mysql-password'), getenv('mysql-db'), 3306);
+  }
 }
--- server/index.php
+++ server/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "hi bro";
+//show all php errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+require './humblimage/images.php';
+
+$test = new images();
+
+print_r(json_encode($test->SearchbyImageId("dVvLmfd6K_k")));
